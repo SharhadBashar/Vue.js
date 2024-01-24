ea8afc9f2253642292e4909c6fecedc6fb72b456
Sharhad Bashar <sharhad.bashar@us0mcb009.targetspot.ny>
2024-01-24 14:16:42 -0500

    finished tutorial 3

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Learning Vue 3
 
 Tutorials:
-https://www.youtube.com/watch?v=YrxBCBibVo0&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=1&ab_channel=NetNinja
+1. https://www.youtube.com/watch?v=YrxBCBibVo0&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=1&ab_channel=NetNinja
--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const app = Vue.createApp({
     data() {
         return {
-            'show': false,
-            'title': 'some title',
-            'age': 10
+            show: false,
+            title: 'some title',
+            age: 10,
+            x: 0,
+            y: 0,
+            books: [
+                {
+                    title: 'name1',
+                    author: 'author1'
+                },
+                {
+                    title: 'name2',
+                    author: 'author2'
+                },
+                {
+                    title: 'name3',
+                    author: 'author3'
+                }
+            ]
         }
     },
     methods: {
         change_age() {
             this.age++
             this.show = !this.show
+        },
+        handle_event(e, data) {
+            console.log(e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handle_mousemove (e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
-        
+    },
+    computed: {
+        filter_books() {
+            return this.books.filter((books) => books.title === 'name1')
+        }
     }
 })
 
